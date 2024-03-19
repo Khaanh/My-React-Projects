90965eb9dd1bc9d64278f1caae9d6f582ca05828
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-19 15:03:37 +0100

    star-rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // import "./App.css";
 import Accordion from "./components/accordion";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
 	return (
@@ -10,7 +11,10 @@ function App() {
 			{/* <Accordion /> */}
 
 			{/* Random color component */}
-			<RandomColor />
+			{/* <RandomColor /> */}
+
+			{/* Star rating */}
+			<StarRating numberOfStars={10} />
 		</div>
 	);
 }
