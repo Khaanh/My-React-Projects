0cd751505b263a9e276b18d6ebf7a5d5412befc8
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-04-02 15:04:55 +0200

    Button load is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 import Accordion from "./components/accordion";
 import ImageSlider from "./components/image-slider";
+import LoadMoreBtn from "./components/load-more-btn";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -18,7 +17,10 @@ function App() {
 			{/* <StarRating numberOfStars={10} /> */}
 
 			{/* Imageslider */}
-			<ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
+			{/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
+
+			{/* LoadMoreBtn */}
+			<LoadMoreBtn />
 		</div>
 	);
 }
